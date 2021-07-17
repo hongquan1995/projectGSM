e0c597dd18a5db254ee6db4dec3fe2bdcde8d78e
Dau Hong Quan <echcom1995@gmail.com>
2021-07-17 09:40:51 +0700

    first commit

---
+++ Wsp_1/CodeProject/Core/Inc/trace.h
@@ -0,0 +1,16 @@
+/*
+ * trace.h
+ *
+ *  Created on: Nov 16, 2020
+ *      Author: 0
+ */
+
+#ifndef INC_TRACE_H_
+#define INC_TRACE_H_
+
+#include <stdint.h>
+
+void my_printf(const char *fmt, ...);
+void printArray(uint8_t array[],uint16_t len );
+
+#endif /* INC_TRACE_H_ */
