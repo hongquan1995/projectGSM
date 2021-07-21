924b577cae95accc2917c1667086539279bccf05
Dau Hong Quan <echcom1995@gmail.com>
2021-07-21 14:04:43 +0700

    change ver1

--- Wsp_1/CodeProject/Core/Inc/L506.h
+++ Wsp_1/CodeProject/Core/Inc/L506.h
@@ -8,6 +8,8 @@
 #ifndef INC_L506_H_
 #define INC_L506_H_
 
+#define 	CMD_PWNON		-1
+
 typedef uint8_t (*fncProcess)(char*);
 
 typedef struct{
@@ -21,22 +23,23 @@ typedef struct{
 	fncProcess fncType;
 }t_uartAt;
 
+
 typedef enum
 {
-	CMD_AT = 0,
-	CMD_CPIN,
-	CMD_CSQ,
-	CMD_CGREG,
-	CMD_CREG,
-	CMD_CGATT,
+	CHECK_CMD_AT = 0,
+	CHECK_STATUS_SIM,
+	CHECK_CMD_CSQ,
+	CHECK_STATUS_NETWORK,
+	CMD_REPORT_NETWORK,
+	CHECK_ATTACHED_STATUS,
 	CMD_CIPTIMEOUT,
-	CMD_CIPMODE,
-	CMD_NETOPEN,
-	CMD_IPADDR,
-	CMD_CIPOPEN,
-	CMD_CIPOPQUERY,
-	CMD_CIPSEND,
-	CMD_CIPRXGET,
+	CHECK_MODE_TCP,
+	CHECK_CMD_NETOPEN,
+	CMD_GET_IPADDR,
+	CMD_CREATE_TCP,
+	CHECK_CMD_CIPOPQUERY,
+	CMD_SEND_DATA,
+	CMD_RECEIVE_DATA,
 	CMD_REVPROCESS,
 	CMD_TRANSRTC
 }cmd_sim4G;
@@ -56,9 +59,13 @@ uint8_t fnParseCGATTPacket(char* arrRes);
 uint8_t fnParseIPADDRPacket(char* arrRes);
 uint8_t fnParseCIPOPQUERYPacket(char* arrRes);
 uint8_t fnParseSendSVPacket(char* arrRes);
+uint8_t fnCheckSendSVPacket(char* arrRes);
 uint8_t fnParseReceiveSVPacket(char* arrRes);
 
 uint8_t fnCheckPacket(uint8_t* packet, uint16_t len, fncProcess fnParse);
 void GPRS_Ask(uint8_t cmd[], uint8_t len);
+void fncSend_CommandAT(uint8_t curr_cmd, uint8_t next_cmd);
+uint8_t fncSend_DataServer(uint8_t curr_cmd, uint8_t *arrSend, uint32_t len);
+void  fncReceive_DataServer(uint8_t curr_cmd);
 
 #endif /* INC_L506_H_ */
